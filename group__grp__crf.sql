e08c66a88e8bc3c0bb74dc36b824c8eafd92898b
Yi Wang <e@yi-wang.me>
2014-10-14 16:27:00 -0400

    group__grp__quantile.sql

---
+++ group__grp__crf.sql
@@ -0,0 +1,1 @@
+﻿--- http://doc.madlib.net/latest/group__grp__crf.html
---
+++ group__grp__quantile.sql
@@ -0,0 +1,14 @@
+﻿--- http://doc.madlib.net/latest/group__grp__quantile.html
+CREATE TABLE tab1 AS SELECT generate_series(1, 1000) AS col1;
+
+select * from tab1;
+
+SELECT madlib.quantile( 'tab1',
+                 'col1',
+                 .9
+               );
+
+SELECT madlib.quantile_big( 'tab1',
+                 'col1',
+                 .8
+               );               
